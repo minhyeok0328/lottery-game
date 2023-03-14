10022d3e5ac8821624cbb2c5045be5c4038cfed2
minhyeok0328 <alsgur7408@gmail.com>
2023-03-15 01:36:24 +0900

    feat: eslint 규칙 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    semi: [2, 'always'],
+    'object-curly-spacing': ['error', 'always'],
+    quotes: ['error', 'single'],
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'eol-last': ['error', 'always'],
+  },
 };
